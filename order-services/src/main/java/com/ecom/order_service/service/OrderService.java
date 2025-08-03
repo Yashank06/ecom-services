01fce6bf7415301a6fee9456cb6952067c1c6aa2
Yashank Maski <maskiyashank88@gmail.com>
2025-08-03 14:51:58 +0530

    Added a new module discovery server

--- order-services/src/main/java/com/ecom/order_service/service/OrderService.java
+++ order-services/src/main/java/com/ecom/order_service/service/OrderService.java
@@ -7,6 +7,7 @@ import com.ecom.order_service.entity.Order;
 import com.ecom.order_service.entity.OrderLineItems;
 import com.ecom.order_service.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -15,12 +16,13 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class OrderService {
 
     private final OrderRepository orderRepository;
-    private final WebClient webClient;
+    private final WebClient.Builder webClientBuilder;
 
     @Transactional
     public void placeOrder(OrderRequest orderRequest){
@@ -31,7 +33,7 @@ public class OrderService {
 
         List<String> skuCodeList = order.getOrderLineItemsList().stream().map(OrderLineItems::getSkuCode).toList();
 
-        InventoryResponse[] inventoryResponseArray = webClient.get().uri("http://localhost:8082/api/inventory",
+        InventoryResponse[] inventoryResponseArray = webClientBuilder.build().get().uri("http://inventory-service/api/inventory",
                         uriBuilder -> uriBuilder.queryParam("skuCodes",skuCodeList).build())
                         .retrieve().bodyToMono(InventoryResponse[].class).block();
 
@@ -39,6 +41,7 @@ public class OrderService {
 
         if(result){
             orderRepository.save(order);
+            log.info("Order saved");
         } else {
             throw new IllegalArgumentException("Product is not in stock, please try again later");
         }
